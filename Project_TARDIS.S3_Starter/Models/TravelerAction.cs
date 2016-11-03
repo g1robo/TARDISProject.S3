794deec02d4a483d8ec9926256eb3e76ff42d795
g1robo <robinsg2@ferris.edu>
2016-11-03 10:23:56 -0400

    Add files via upload

---
+++ Project_TARDIS.S3_Starter/Models/TravelerAction.cs
@@ -0,0 +1,22 @@
+﻿namespace Project_TARDIS
+{
+    public enum TravelerAction
+    {
+        None,
+        MissionSetup,
+        LookAround,
+        LookAt,
+        PickUpItem,
+        PickUpTreasure,
+        PutDownItem,
+        PutDownTreasure,
+        Travel,
+        TravelerInfo,
+        TravelerInventory,
+        TravelerTreasure,
+        ListTARDISDestinations,
+        ListItems,
+        ListTreasures,
+        Exit
+    }
+}
